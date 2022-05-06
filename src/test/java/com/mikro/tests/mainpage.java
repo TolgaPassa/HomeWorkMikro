0e32059b4180e1a991a45af9c26ca4e90da0e8b4
Tolga Paşaalioğlu <pasa198299@gmail.com>
2022-05-06 23:38:37 +0300

    new

--- src/test/java/com/mikro/tests/mainpage.java
+++ src/test/java/com/mikro/tests/mainpage.java
@@ -14,6 +14,9 @@ import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 import utilities.WebDriverFactory;
 
+import java.awt.*;
+import java.security.Key;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class mainpage {
@@ -54,7 +57,7 @@ public class mainpage {
             System.out.println("actualUrl = " + actualUrl);
             System.out.println("expectedURl = " + expectedURl);
         }
-         */
+  */
 
         String username = ConfigurationReader.get("driver_username");
         String password = ConfigurationReader.get("driver_password");
@@ -63,7 +66,8 @@ public class mainpage {
 
         Assert.assertEquals("Giriş Yap - n11.com",driver.getTitle());
 
-        driver.findElement(By.id("email")).clear();
+        //normalde login izin verilmesi durumunda olucak sistem
+        /*driver.findElement(By.id("email")).clear();
         driver.findElement(By.name("email")).sendKeys(username);
 
         driver.findElement(By.name("password")).clear();
@@ -73,11 +77,68 @@ public class mainpage {
         Thread.sleep(1000);
 
         driver.findElement(By.id("loginButton")).click();
-        Assert.assertEquals("n11.com",driver.getTitle());
+        Assert.assertEquals("Giriş Yap - n11.com",driver.getTitle());
 
 
         //bildirim kapatma
         driver.findElement(By.xpath("//*[@id=\"dengage-push-perm-slide\"]/div/div[2]/div/button[2]")).click();
+*/
+
+        driver.findElement(By.xpath("//*[@id=\"loginContainer\"]/div/div[1]/div/div[3]")).click();
+
+        String currentWindowHandle = driver.getWindowHandle();
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String handle : windowHandles) {
+            if (!handle.equals(currentWindowHandle)){
+                driver.switchTo().window(handle);
+            }
+        }
+
+
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(username);
+        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys(password);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
+        Thread.sleep(8000);
+
+        driver.switchTo().window(currentWindowHandle);
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/div[2]/div[1]")).click();
+
+
+        System.out.printf( driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[2]/div[2]/div[1]")).getText());
+
+        driver.findElement(By.xpath("//*[@id=\"searchData\"]")).sendKeys("Iphone");
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[1]/div/a")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals("Iphone - n11.com",driver.getTitle());
+        System.out.printf(driver.findElement(By.xpath("//*[@id=\"contentListing\"]/div/div/div[2]/section[2]/div[1]/div[3]")).getText());
+
+        driver.findElement(By.xpath("//*[@id=\"searchData\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"searchData\"]")).sendKeys("Ayfon");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[1]/div/a")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals("Ayfon - n11.com",driver.getTitle());
+        System.out.printf(driver.findElement(By.xpath("//*[@id=\"contentListing\"]/div/div/div[2]/section[2]/div[1]/div[3]")).getText());
+
+        driver.findElement(By.xpath("//*[@id=\"searchData\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"searchData\"]")).sendKeys("iphone");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div[2]/div[1]/div/a")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals("Iphone - n11.com",driver.getTitle());
+        System.out.printf(driver.findElement(By.xpath("//*[@id=\"contentListing\"]/div/div/div[2]/section[2]/div[1]/div[3]")).getText());
+
+
+
+
+
+
+
+
+
 
 
     }
